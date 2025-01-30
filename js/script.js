e1534ad0160ca596de597ae6f64413625ce7d9b1
Jan <1673276@uab.cat>
2025-01-30 22:26:12 +0100

    Funcio fetch

--- js/script.js
+++ js/script.js
@@ -1,4 +1,12 @@
-// Un script senzill per mostrar un missatge al carregament de la pàgina
-document.addEventListener("DOMContentLoaded", function() {
-    console.log("La pàgina s'ha carregat completament!");
-});
+// scripts.js
+
+// Funció per carregar el contingut del fitxer header.html a l'element amb id "navbar"
+window.onload = function() {
+    // Utilitzem Fetch API per carregar el contingut del fitxer header.html
+    fetch('header.html')
+        .then(response => response.text()) // Convertim la resposta en text (HTML)
+        .then(data => {
+            document.getElementById('navbar').innerHTML = data; // Carreguem el contingut al div amb id="navbar"
+        })
+        .catch(error => console.error('Error carregant el header:', error)); // Si hi ha error, el mostrem
+};
