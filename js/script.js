c2f9f3e6a6c63562d4e28e72b67cec6a384c62a2
Jan <1673276@uab.cat>
2025-01-30 22:07:04 +0100

    First Commit

---
+++ js/script.js
@@ -0,0 +1,4 @@
+// Un script senzill per mostrar un missatge al carregament de la pàgina
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("La pàgina s'ha carregat completament!");
+});
